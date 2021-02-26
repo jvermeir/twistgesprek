2b6c5ba3cd283523892564d2e631c820e88ac8a3
Arjan Molenaar <gaphor@gmail.com>
2021-02-26 15:16:04 +0100

    Use __main__ (dunder-main)

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+
 def read_json_index(folder):
     path = Path(folder)
     index = {}
@@ -8,6 +9,7 @@ def read_json_index(folder):
         with open(f) as of:
             index[f.name.split(".")[0]] = json.load(of)
     return index
+
 
 def read_text_index(folder):
     path = Path(folder)
@@ -18,8 +20,13 @@ def read_text_index(folder):
     return index
 
 
-entity_sentiment = read_json_index("./db/entity-sentiment")
-sentiment = read_json_index("./db/sentiment")
-text = read_text_index("./db/text")
+def main():
+    entity_sentiment = read_json_index("./db/entity-sentiment")
+    sentiment = read_json_index("./db/sentiment")
+    text = read_text_index("./db/text")
 
-print(len(entity_sentiment), len(sentiment), len(text))
+    print(len(entity_sentiment), len(sentiment), len(text))
+
+
+if __name__ == "__main__":
+    main()
